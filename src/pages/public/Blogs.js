92fda2ad51b125bb9ea20792fd31786cb9c35ad5
Kiet Truong <kietb2016977@student.ctu.edu.vn>
2023-11-02 22:34:13 +0700

    blog,faqs

--- src/pages/public/Blogs.js
+++ src/pages/public/Blogs.js
@@ -1,7 +1,149 @@
+import { Breadcrumb } from "components";
 import React from "react";
-
+import blogImg1 from "assets/Blog/blog1.png";
 const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <div className="w-full">
+      <div className="h-[81px] flex  justify-center items-center bg-gray-100">
+        <div className="w-main">
+          <h3 className="font-semibold uppercase mb-2">Blog</h3>
+          <Breadcrumb></Breadcrumb>
+        </div>
+      </div>
+      <div className="w-main m-auto flex items-center justify-center mt-10">
+        <h2 className="text-main text-4xl font-semibold">Blogs Of Shoes</h2>
+      </div>
+      <div className="w-main mt-8 m-auto grid grid-cols-3 gap-8">
+        <div className=" shadow-lg rounded-lg">
+          <img
+            src={blogImg1}
+            className="w-full rounded-tl-lg rounded-tr-lg h-[300px]  object-cover"
+          ></img>
+          <h2 className=" text-center mt-4 font-medium text-lg uppercase">
+            title
+          </h2>
+          <span className=" text-gray-500 line-clamp-[7] p-2 text-justify mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nec
+            tincidunt praesent semper feugiat nibh sed. Quis auctor elit sed
+            vulputate mi sit amet. Facilisis mauris sit amet massa. Sit amet
+            porttitor eget dolor morbi non. Odio aenean sed adipiscing diam
+            donec adipiscing tristique risus. Turpis egestas pretium aenean
+            pharetra magna ac placerat vestibulum. Pharetra pharetra massa massa
+            ultricies mi quis hendrerit dolor. Ipsum faucibus vitae aliquet nec
+            ullamcorper sit. Purus in mollis nunc sed id semper risus in
+            hendrerit.
+          </span>
+        </div>
+        <div className=" shadow-lg rounded-lg">
+          <img
+            src={blogImg1}
+            className="w-full rounded-tl-lg rounded-tr-lg h-[300px]  object-cover"
+          ></img>
+          <h2 className=" text-center mt-4 font-medium text-lg uppercase">
+            title
+          </h2>
+          <span className=" text-gray-500 line-clamp-[7] p-2 text-justify mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nec
+            tincidunt praesent semper feugiat nibh sed. Quis auctor elit sed
+            vulputate mi sit amet. Facilisis mauris sit amet massa. Sit amet
+            porttitor eget dolor morbi non. Odio aenean sed adipiscing diam
+            donec adipiscing tristique risus. Turpis egestas pretium aenean
+            pharetra magna ac placerat vestibulum. Pharetra pharetra massa massa
+            ultricies mi quis hendrerit dolor. Ipsum faucibus vitae aliquet nec
+            ullamcorper sit. Purus in mollis nunc sed id semper risus in
+            hendrerit.
+          </span>
+        </div>
+        <div className=" shadow-lg rounded-lg">
+          <img
+            src={blogImg1}
+            className="w-full rounded-tl-lg rounded-tr-lg h-[300px]  object-cover"
+          ></img>
+          <h2 className=" text-center mt-4 font-medium text-lg uppercase">
+            title
+          </h2>
+          <span className=" text-gray-500 line-clamp-[7] p-2 text-justify mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nec
+            tincidunt praesent semper feugiat nibh sed. Quis auctor elit sed
+            vulputate mi sit amet. Facilisis mauris sit amet massa. Sit amet
+            porttitor eget dolor morbi non. Odio aenean sed adipiscing diam
+            donec adipiscing tristique risus. Turpis egestas pretium aenean
+            pharetra magna ac placerat vestibulum. Pharetra pharetra massa massa
+            ultricies mi quis hendrerit dolor. Ipsum faucibus vitae aliquet nec
+            ullamcorper sit. Purus in mollis nunc sed id semper risus in
+            hendrerit.
+          </span>
+        </div>
+        <div className=" shadow-lg rounded-lg">
+          <img
+            src={blogImg1}
+            className="w-full rounded-tl-lg rounded-tr-lg h-[300px]  object-cover"
+          ></img>
+          <h2 className=" text-center mt-4 font-medium text-lg uppercase">
+            title
+          </h2>
+          <span className=" text-gray-500 line-clamp-[7] p-2 text-justify mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nec
+            tincidunt praesent semper feugiat nibh sed. Quis auctor elit sed
+            vulputate mi sit amet. Facilisis mauris sit amet massa. Sit amet
+            porttitor eget dolor morbi non. Odio aenean sed adipiscing diam
+            donec adipiscing tristique risus. Turpis egestas pretium aenean
+            pharetra magna ac placerat vestibulum. Pharetra pharetra massa massa
+            ultricies mi quis hendrerit dolor. Ipsum faucibus vitae aliquet nec
+            ullamcorper sit. Purus in mollis nunc sed id semper risus in
+            hendrerit.
+          </span>
+        </div>
+        <div className=" shadow-lg rounded-lg">
+          <img
+            src={blogImg1}
+            className="w-full rounded-tl-lg rounded-tr-lg h-[300px]  object-cover"
+          ></img>
+          <h2 className=" text-center mt-4 font-medium text-lg uppercase">
+            title
+          </h2>
+          <span className=" text-gray-500 line-clamp-[7] p-2 text-justify mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nec
+            tincidunt praesent semper feugiat nibh sed. Quis auctor elit sed
+            vulputate mi sit amet. Facilisis mauris sit amet massa. Sit amet
+            porttitor eget dolor morbi non. Odio aenean sed adipiscing diam
+            donec adipiscing tristique risus. Turpis egestas pretium aenean
+            pharetra magna ac placerat vestibulum. Pharetra pharetra massa massa
+            ultricies mi quis hendrerit dolor. Ipsum faucibus vitae aliquet nec
+            ullamcorper sit. Purus in mollis nunc sed id semper risus in
+            hendrerit.
+          </span>
+        </div>
+        <div className=" shadow-lg rounded-lg">
+          <img
+            src={blogImg1}
+            className="w-full rounded-tl-lg rounded-tr-lg h-[300px]  object-cover"
+          ></img>
+          <h2 className=" text-center mt-4 font-medium text-lg uppercase">
+            title
+          </h2>
+          <span className=" text-gray-500 line-clamp-[7] p-2 text-justify mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna nec
+            tincidunt praesent semper feugiat nibh sed. Quis auctor elit sed
+            vulputate mi sit amet. Facilisis mauris sit amet massa. Sit amet
+            porttitor eget dolor morbi non. Odio aenean sed adipiscing diam
+            donec adipiscing tristique risus. Turpis egestas pretium aenean
+            pharetra magna ac placerat vestibulum. Pharetra pharetra massa massa
+            ultricies mi quis hendrerit dolor. Ipsum faucibus vitae aliquet nec
+            ullamcorper sit. Purus in mollis nunc sed id semper risus in
+            hendrerit.
+          </span>
+        </div>
+      </div>
+      <div className="h-[500px]"></div>
+    </div>
+  );
 };
 
 export default Blogs;
